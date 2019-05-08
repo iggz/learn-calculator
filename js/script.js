4b9c402f9cc3782d0f33cc98a89c7fc0ac7e1493
igor popenov <ipopenov@protonmail.com>
2019-05-08 16:48:53 -0400

    Added numbers to input, one operator at a time.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const input = document.getElementById('input'), // input/output button
-    number = document.querySelectorAll('.numbers div'), // number buttons
-    operator = document.querySelectorAll('.operators div'), // operator buttons
+    numbers = document.querySelectorAll('.numbers div'), // number buttons
+    operators = document.querySelectorAll('.operators div'), // operator buttons
     result = document.getElementById('result'), // equal button
     clear = document.getElementById('clear'); // clear button
     
@@ -10,8 +10,70 @@ let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
 // adding click handlers to number buttons
 
+numbers.forEach(function(number){
+    number.addEventListener('click', function() {
+        input.innerHTML += this.innerHTML;
+        let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length - 1];
+        console.log(currentString);
+        console.log(lastChar);
+    });
+});
+
 // adding click handlers to the operation buttons
+
+operators.forEach(function(operator){
+    operator.addEventListener('click', function() {
+        // input.innerHTML += this.innerHTML;  #this made the function not work
+        let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length - 1];
+        if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷"){
+            let newString = currentString.substring(0, currentString.length - 1) + this.innerHTML;
+            input.innerHTML = newString;
+        } else if (currentString.length == 0){
+            console.log("enter a number first");
+        } else {
+            input.innerHTML += this.innerHTML;
+        }
+        console.log(currentString);
+        console.log(lastChar);
+    });
+});
 
 // on click of 'equal' button
 
+result.addEventListener('click', function() {
+    input.innerHTML = '';
+});
+
 // clearing the input on press of clear
+
+clear.addEventListener('click', function() {
+    input.innerHTML = '';
+});
+
+    /*
+    (/\+|\-|\×|\÷/g)
+
+    // use like this:
+    const someVariable = someString.split(/\+|\-|\×|\÷/g);
+    */
+
+
+//    const string = "12 +";
+//    const lastCharacter = string[string.length - 1];
+//    console.log(lastCharacter);
+//    const mathString = "12+2+3-4";
+//    const numbers = mathString.split(/\+|\-|\Ã|\Ã·/g);
+//    console.log(numbers);
+//    const months = ['Jan', 'March', 'April', 'June'];
+//    // array.splice(start, deleteCount, item);
+//    // inserts at index 1
+//    months.splice(1, 0, 'Feb');
+//    months.splice(4, 0, 'May');
+//    console.log(months);
+//    // replaces 1 element at index 4
+//    months.splice(5, 1, 'July');
+//    console.log(months);
+//    const firstMonth = months.indexOf('April');
+//    console.log(firstMonth);
